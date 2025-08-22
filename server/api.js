c5fbf0362f0c87715b586f8157f156b4c47aa285
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2025-08-22 13:06:01 +0700

    perbaiki konflik penggabungan dan tambahkan fungsi cetak

--- server/api.js
+++ server/api.js
@@ -2,17 +2,16 @@ function coba() {
   console.log("Coba");
 }
 
-function Penjumlahan(i) {
-  i += i;
-  console.log(i);
-}
-
-function pesan() {
-  console.log("Hallo Vandra");
-}
-
 coba();
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+function cetak() {
+  console.log("aku vandra");
+}
+cetak();
+>>>>>>> 9b510c687a8f8b9ad21b8427547abba1b5089f88
 Penjumlahan(10);
 pesan();
 
